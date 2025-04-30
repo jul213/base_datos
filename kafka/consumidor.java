f27cbc8d206bb8f522dc741a767944be6e2074e3
julio <158296107+jul213@users.noreply.github.com>
2025-04-30 10:15:23 +0000

    importaciones

--- kafka/consumidor.java
+++ kafka/consumidor.java
@@ -6,3 +6,11 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.List;
 
+import org.apache.kafka.clients.consumer.ConsumerRecords;
+import org.apache.kafka.common.header.Header;
+import org.apache.kafka.common.TopicPartition;
+import org.apache.kafka.clients.consumer.ConsumerRebalanceListener;
+import org.apache.kafka.clients.consumer.consumerRecord;
+import org.oracle.okafka.clients.consumer.KafkaConsumer;
+
+
