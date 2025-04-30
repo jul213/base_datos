cac601c848f7a70de4ec21a08d203082d7068c85
julio <158296107+jul213@users.noreply.github.com>
2025-04-30 09:34:54 +0000

    importaciones

--- kafka/consumidor.java
+++ kafka/consumidor.java
@@ -2,4 +2,7 @@ import java.util.Properties;
 import java.time.Duration;
 import java.time.Instant;
 import java.util.ArrayList;
-import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.List;
+
