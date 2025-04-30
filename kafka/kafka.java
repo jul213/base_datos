b087095d46bf3327523c61a1b115349cb7406cfa
julio <158296107+jul213@users.noreply.github.com>
2025-04-30 06:54:25 +0000

    terminamos las importaciones del programa kafka

--- kafka/kafka.java
+++ kafka/kafka.java
@@ -3,3 +3,8 @@ import java.util.Properties;
 import java.util.concurrent.ExecutionException;
 
 
+import org.apache.kafka.clients.admin.Admin;
+import org.apache.kafka.clients.admin.CreateTopicsResult;
+import org.apache.kafka.clients.admin.NewTopic;
+import org.apache.kafka.clients.common.kafkaFuture;
+
